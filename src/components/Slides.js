bfb6d7d7f7b61611bd9e8b379161f61cbca985b3
Preeti Sharma <preetisharmabtech100@gmail.com>
2021-02-23 12:55:06 +0530

    adds logic for action buttons in slide

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 function Slides({slides}) {
 
+    const [currentSlide, setCurrenSlide] = useState(0);
+
+    const next = useCallback(() => {
+        setCurrenSlide(currentSlide => currentSlide + 1);
+    }, []);
+
+    const previous = useCallback(() => {
+        setCurrenSlide(currentSlide => currentSlide - 1);
+    }, []);
+    const reset = useCallback(() => {
+        setCurrenSlide(0);
+    }, []);
+
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button  data-testid="button-restart" className="small outlined" onClick={reset}>Restart</button>
+                <button disabled={currentSlide === 0} data-testid="button-prev" className="small" onClick={previous}>Prev</button>
+                <button disabled={currentSlide === slides.length - 1} data-testid="button-next" className="small" onClick={next}>Next</button>
             </div>
             <div id="slide" className="card text-center">
                 <h1 data-testid="title">Slide Title Here</h1>
