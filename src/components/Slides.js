d8366eedebd64c6ac0c23d5b73f5062bd718d6e9
Preeti Sharma <preetisharmabtech100@gmail.com>
2021-03-01 21:47:11 +0530

    implements functionality

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+/**
+ * @param {object} props
+ * @param {{
+ *   title: string, text: string
+ * }[]}  props.slides
+ */
 function Slides({slides}) {
+    const [slideIndex, setSlideIndex] = useState(0)
+    const title = slides[slideIndex].title
+    const text = slides[slideIndex].text
 
+    function handleRestart() {
+        setSlideIndex(0)
+    }
+    function handlePrev() {
+        setSlideIndex(slideIndex-1)
+    }
+    function handleNext() {
+        if(slideIndex < slides.length-1)
+            setSlideIndex(slideIndex+1)
+    }
+
+    
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button disabled={slideIndex === 0} data-testid="button-restart" className="small outlined" onClick={handleRestart}>Restart</button>
+                <button disabled={slideIndex === 0} data-testid="button-prev"  className="small" onClick={handlePrev}>Prev</button>
+                <button disabled={slideIndex === slides.length-1} data-testid="button-next" className="small" onClick={handleNext}>Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+            <h1 data-testid="title">{title}</h1>
+            <p data-testid="text">{text}</p>
             </div>
         </div>
     );
